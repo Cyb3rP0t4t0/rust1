2357061271dd9a28cba3a1efc538046018f37d3d
Cyb3rP0t4t0 <officialcyb3rhq@gmail.com>
2023-01-08 21:40:15 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main() {
-    let mut x = 20;
+    /*let mut x = 20;
     println!("\nx is: {}", x); //It's a macro, I don't even now what macro means :'(
         
         { //This is a scope, cool shit. Everything you write will stay inside in this scope. And you can use variables from the outside like:
@@ -28,4 +28,49 @@ fn main() {
 
     io::stdin().read_line(&mut input).expect("failed line");
     println!("\n{}", input);
+
+    let z = 100_000i64; //What type of variable it is going to be. Also able to use _ like 10_i8 e.t.c / and as
+    let u = i8::MAX as i32; //Gonna be the 8bit's max
+
+    println!("{}", z / (u as i64)); //Converting from i32 to i64
+
+    let mut input1 = String::new();
+
+    io::stdin().read_line(&mut input1).expect("Oopsi");
+    let int_input: i64 = input1.trim().parse().unwrap();
+
+    println!("{}", int_input);
+    
+    let cond = 2 > 3;
+
+    let cond2 = cond && true;
+
+    println!("{}", cond2);*/
+
+    let food = "cookie";
+
+    if food == "cookie" {
+        println!("\nIt is cookie!");
+    } else {
+        println!("\nIt is not cookie...");
+    }
+
+    test();
+    calc(8,34);
+
+    let num = { //statement can hold an expression
+        let x = 3;
+        x + 1 //Don't close it cuz we wanna return it!
+    };
+
+    println!("{}", num);
 }
+
+fn test() {
+    println!("\nTested!");
+}
+
+fn calc(x: i64, y: i64) {
+    println!("\nSum: {}", x + y);
+}
+
